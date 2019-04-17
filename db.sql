85da73895513196cb71a8f4ecd1f0207f308368c
Xiaodan.C <CiasaCoder@gmail.com>
2019-04-17 20:10:25 +0800

    update

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE `employee`
 (
     `id`   int(6) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(30)     NOT NULL,
-    `city` varchar(30)     not null,
+    `city` varchar(30)     NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbPass := ""
-	dbName := "db_gsql"
+	dbName := "db_employee"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
 		fmt.Printf("open db error:%v\n", err.Error())
